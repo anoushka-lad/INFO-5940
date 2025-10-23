89b59f53034d5e480cc263be4ada65ccc8f98480
anoushka-lad <al2742@cornell.edu>
2025-10-23 11:57:53 -0400

    streamlit commit before codespace is deleted

--- streamlit_sample.py
+++ streamlit_sample.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from openai import OpenAI
+client = OpenAI()
 
 st.set_page_config(page_title="Hello Codespaces", layout="centered")
 
@@ -7,7 +9,26 @@ st.write("If you can see this page, Streamlit is running correctly inside your C
 
 name = st.text_input("What is your name?")
 if name:
-    st.success(f"Nice to meet you, {name}!")
+   st.success(f"Nice to meet you, {name}!")
 
 st.markdown("---")
 st.caption("This is a test app for INFO 5940 Fall 2025.")
+
+if "messages" not in st.session_state:
+   st.session_state["messages"] = [{"role": "assistant", "content": "Howdy!"}]
+
+
+for msg in st.session_state.messages:
+   if msg['role'] != 'system':
+       st.chat_message(msg['role']).write(msg['content'])
+
+
+if prompt := st.chat_input():
+   st.session_state.messages.append({"role": "user", "content": prompt})
+   st.chat_message("user").write(prompt)
+
+   with st.chat_message("assistant"):
+       stream = client.chat.completions.create(model = "openai.gpt-4o", messages = st.session_state.messages, stream = True)
+       response = st.write_stream(stream)
+
+   st.session_state.messages.append({"role": "assistant", "content": response})
